a864cd0e3091f9ea109eb0612a4fef4ab9bbde9a
IlanTobal <ilantobal@gmail.com>
2021-12-02 17:13:46 -0300

    Binario

---
+++ Binario/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Binario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(33, 68);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(39, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Presione";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Binario.Properties.Resources.gfgfdg;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 454);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Binario/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Binario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Reproducir(string nota, string octava)
+        {
+            // Funcion que reproduce las notas.
+            Stream str = (Stream)Properties.Resources.ResourceManager.GetObject(nota.Replace("#", "S") + octava);
+            SoundPlayer Sonido = new SoundPlayer(str);
+            Sonido.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            analizarArduino();
+            /*
+            string input = (txtValor.Text); //se guarda la informacion que le llega desde el arduino, en este caso lo qu3e recibe desde el textbox. 
+            string[] notas = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+
+            for (int l = 13; l < 17; l++)  
+            {
+                string laser = input.Substring(l, 1); // declara que laser fue interumpido 
+                if (laser == "1")
+                {
+                    for (int i = 0; i < 13; i++)
+                    {
+                        string btn = input.Substring(i, 1); // declara que boton fue precionado 
+                        if (btn == "1")
+                        {
+                            Reproducir(notas[i], (l-12).ToString()); //ejecuta la funcion 
+
+                            MessageBox.Show(notas[i]+(l-12).ToString());
+                        }
+                    }
+                }
+            }*/
+        }
+
+        void analizarArduino ()
+        {
+            string input = (txtValor.Text);
+            
+            string[] notas = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+            // int[] numeros = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            // string[] letrasCuerdas = { "E", "A", "D", "G" };
+            int[] valoresCuerdas = { 4, 9, 3, 7 };
+
+            List<int> presionados = new List<int>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (input[i] == '1') presionados.Add(i+1);
+            }
+
+            for (int i = 12; i < 16; i++)
+            {
+                if (input[i] == '1')
+                {
+                    int valorCuerda = i - 12;
+                    int numeroSonido = valoresCuerdas[valorCuerda];
+
+                    bool toqueAlguna = false;
+
+                    foreach (int presionado in presionados)
+                    {
+                 
+                        toqueAlguna = true;
+                        numeroSonido += presionado;
+                        Reproducir(notas[numeroSonido % 12], (numeroSonido / 12 + 1).ToString());
+                        MessageBox.Show(notas[numeroSonido % 12] + (numeroSonido / 12 + 1).ToString());
+
+                        // 1000000000001000
+                    }
+
+                    if (!toqueAlguna)
+                    {
+                        Reproducir(notas[valoresCuerdas[valorCuerda]], (numeroSonido / 12 + 1).ToString());
+                        MessageBox.Show(notas[valoresCuerdas[valorCuerda]] + (numeroSonido / 12 + 1).ToString());
+                    }
+                }
+            }
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
